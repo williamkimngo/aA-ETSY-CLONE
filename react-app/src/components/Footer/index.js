69faecd5b598b47d63fad8b9739a0663524587f6
William Ngo <williamkimngo@gmail.com>
2022-11-30 22:18:32 -0800

    footer

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './footer.css'
+
+const CreaterLinks = () => {
+    return (
+        <div className="footer">
+            <div className="github-links">
+                <a href="https://github.com/williamkimngo">
+                    <i class="fa-brands fa-github">
+                        <div className="names-git">William Ngo</div>
+                    </i>
+                </a>
+            </div>
+            <div className="github-links">
+                <a href='https://www.linkedin.com/in/william-ngo-005950254/'>
+                    <i class="fab fa-linkedin-in">
+                        <div className="names-git">William Ngo</div>
+                    </i>
+                </a>
+            </div>
+        </div>
+    )
+}
+export default CreaterLinks
