2462a20a8af18136b84a0b0cbc15194ed30e46c2
William Ngo <williamkimngo@gmail.com>
2023-01-11 12:07:13 -0800

    footer

--- react-app/src/components/ProductHomePage/index.js
+++ react-app/src/components/ProductHomePage/index.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { fetchAllProducts } from '../../store/product';
 import './HomePage.css'
-import CreaterLinks from '../Footer';
+import Footer from '../Footer';
 import { resetProductsState } from '../../store/product';
 
 const ProductHome = () => {
@@ -108,7 +108,7 @@ const ProductHome = () => {
                 </div>
             </div>
             <div className='footer-home-page'>
-            <CreaterLinks/>
+            <Footer/>
             </div>
         </div>
 
